fe86fbfc2d998aba3a888526ecf5d0222f5d9509
Mihira <mihirapatel1@gmail.com>
2020-05-29 17:08:46 -0700

    Return comments data (hard-coded) and add it to page, format and parse JSON

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,18 +15,35 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private List<String> messages;
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Mihira!");
+    // Create ArrayList
+    messages = new ArrayList<>();
+    messages.add("Hi, how are you doing today?");
+    messages.add("Hey, how is your day going?");
+    messages.add("Hello, what is up?");
+
+    // Convert to JSON string
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
